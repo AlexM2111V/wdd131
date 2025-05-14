ea9f9ef66b33e0fb9df862d818e4aed30a33892c
AlexM2111V <jesusmendozav2111@gmail.com>
2025-05-14 18:35:24 -0500

    W01 Assignment: Home Page added

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
